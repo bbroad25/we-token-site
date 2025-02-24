3fe15a44987d256cf902c6ceff2278bff98cf699
bbroad25 <bbroad25@gmail.com>
2025-02-24 15:05:35 -0500

    fix page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-mport { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { motion } from "framer-motion";
 
